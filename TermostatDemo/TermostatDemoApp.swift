40cb649c95b090307df0ddbc3316d2e246959b74
Artem Paliutin <pal.artem@icloud.com>
2022-04-25 23:50:23 +0300

    Initial Commit

---
+++ TermostatDemo/TermostatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermostatDemoApp.swift
+//  TermostatDemo
+//
+//  Created by Artem Paliutin on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TermostatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
