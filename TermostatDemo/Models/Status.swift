eb405c6576d9cd068a66d8fb2676b532e9ede79a
Artem Paliutin <pal.artem@icloud.com>
2022-04-26 14:03:37 +0300

    add ThermometerScaleView

---
+++ TermostatDemo/Models/Status.swift
@@ -0,0 +1,15 @@
+//
+//  Status.swift
+//  TermostatDemo
+//
+//  Created by Artem Paliutin on 26/04/2022.
+//
+
+import Foundation
+
+
+enum Status: String {
+    case heating = "HAETING"
+    case cooling = "COOLING"
+    case reaching = "REACHING"
+}
